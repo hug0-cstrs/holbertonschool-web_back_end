d259058a4e28d23b13f7bbe9d57a40a0c4210ad2
Hugo Castéras <130242027+hug0-cstrs@users.noreply.github.com>
2023-08-28 19:13:46 +0000

    fix pycodestyle error

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -2,6 +2,7 @@
 """type-annotated function sum_list"""
 from typing import List
 
+
 def sum_list(input_list: List[float]) -> float:
     """function that returns their sum as a float"""
     return sum(input_list)
