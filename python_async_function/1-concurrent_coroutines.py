ad2be99613f279ffc7e64e3762d4b959cacc3132
Hugo Castéras <130242027+hug0-cstrs@users.noreply.github.com>
2023-08-29 12:40:49 +0000

    update file

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """A module with a asynchronus routine"""
 import asyncio
+from typing import List
 
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n, max_delay):
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     An asynchronous function that launches the wait_random function 'n'
     times with the specified maximum delay.
