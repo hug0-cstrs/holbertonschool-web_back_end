d8048ffa45d4857a744def5391f0ea1e60366cc2
Hugo Castéras <130242027+hug0-cstrs@users.noreply.github.com>
2023-08-29 19:38:17 +0000

    update file

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
+"""A module with a asynchronous generator"""
+from typing import List
 
 async_generator = __import__("0-async_generator").async_generator
 
 
-async def async_comprehension() -> list[float]:
+async def async_comprehension() -> List[float]:
     """Coroutine collects 10 random numbers using asynchronous understanding"""
     random_numbers = [num async for num in async_generator()]
     return random_numbers
