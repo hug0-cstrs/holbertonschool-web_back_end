f102de1673fcb8fc6bf15a4c830ddbea094de8d1
Hugo Castéras <130242027+hug0-cstrs@users.noreply.github.com>
2023-09-04 13:23:44 +0000

    function that lists all documents in a collection

---
+++ NoSQL/8-all.py
@@ -0,0 +1,8 @@
+"""script that lists all documents in a collection"""
+
+
+def list_all(mongo_collection):
+    """Function that lists all documents in a collection"""
+    if mongo_collection is None:
+        return []
+    return mongo_collection.find()
