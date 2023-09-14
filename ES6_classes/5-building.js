57a2221869529ce42c4c17029280adceab01ea1c
Hugo Castéras <130242027+hug0-cstrs@users.noreply.github.com>
2023-09-14 14:48:08 +0000

    5. A Building

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,18 @@
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building) {
+      if (!this.evacuationWarningMessage) {
+        throw Error('Class extending Building must override evacuationWarningMessage');
+      }
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(newSqft) {
+    this._sqft = newSqft;
+  }
+}
