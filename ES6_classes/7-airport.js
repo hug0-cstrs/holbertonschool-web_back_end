d687063ca96d4a07cb91990177495078c0ab3e84
Hugo Castéras <130242027+hug0-cstrs@users.noreply.github.com>
2023-09-16 09:53:02 +0000

    update file

--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -1,12 +1,10 @@
-class Airport {
+export default class Airport {
   constructor(name, code) {
     this._name = name;
     this._code = code;
   }
 
   toString() {
-    return `[${this._code}]`;
+    return (`[object ${this._code}]`);
   }
 }
-
-export default Airport;
