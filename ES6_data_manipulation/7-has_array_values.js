172d56a5a38c228fa3b0a00f0fba9674f850144f
Hugo Castéras <130242027+hug0-cstrs@users.noreply.github.com>
2023-10-06 12:09:01 +0000

    update file

--- ES6_data_manipulation/7-has_array_values.js
+++ ES6_data_manipulation/7-has_array_values.js
@@ -1,8 +1,3 @@
-export default function hasValuesFromArray(set, array) {
-  for (const element of array) {
-    if (!set.has(element)) {
-      return false;
-    }
-  }
-  return true;
+export default function hasValuesFromArray(aSet, anArray) {
+  return anArray.every((value) => aSet.has(value));
 }
