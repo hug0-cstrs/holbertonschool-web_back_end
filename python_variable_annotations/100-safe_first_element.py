b2da3a71c5c62002b8ce305a8a13b20c9b0511a8
hug0-cstrs <hugo.casteras@outlook.fr>
2024-02-05 18:29:07 +0000

    update file

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -1,19 +1,12 @@
 #!/usr/bin/env python3
-"""
-    Duck typing sequence Any
-"""
+'''
+Return the first element of a list or none
+'''
 from typing import Sequence, Any, Union
 
 
-# The types of the elements of the input are not known
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
-    """
-        Args:
-            lst: Any data type
-
-        Return:
-            None or first element
-    """
+    ''' Return the first element of a list or none '''
     if lst:
         return lst[0]
     else:
