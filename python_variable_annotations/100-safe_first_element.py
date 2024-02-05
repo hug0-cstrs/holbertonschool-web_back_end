6370fc41d5514ea15b3ad2e612356abe5af35b0e
hug0-cstrs <hugo.casteras@outlook.fr>
2024-02-05 18:21:37 +0000

    Fix order of imports in safe_first_element.py

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python3""
+#!/usr/bin/env python3
 """
     Duck typing sequence Any
 """
-from typing import Sequence, Union, Any
+from typing import Any, Sequence, Union
 
 
-# The types of the elements of the input are not known
+# The types of the elements of the input are not know
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """
         Args:
@@ -17,8 +17,4 @@ def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     if lst:
         return lst[0]
     else:
-      return None
-
-
-
-print(safe_first_element.__annotations__)
+        return None
