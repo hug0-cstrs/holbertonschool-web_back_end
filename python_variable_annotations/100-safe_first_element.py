95dc5250e0804db13862852cfc5085e020fbcabb
hug0-cstrs <hugo.casteras@outlook.fr>
2024-02-05 18:24:22 +0000

    udpate file

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -2,10 +2,10 @@
 """
     Duck typing sequence Any
 """
-from typing import Any, Sequence, Union
+from typing import Sequence, Any, Union
 
 
-# The types of the elements of the input are not know
+# The types of the elements of the input are not known
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """
         Args:
