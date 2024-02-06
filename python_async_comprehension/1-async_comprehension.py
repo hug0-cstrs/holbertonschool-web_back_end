9c2917331fa0aae5244fc19d1dcf03967b9738af
hug0-cstrs <hugo.casteras@outlook.fr>
2024-02-06 09:21:13 +0000

    adds comments for a better understanding

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -7,5 +7,5 @@ async_generator = __import__("0-async_generator").async_generator
 
 async def async_comprehension() -> List[float]:
     """Coroutine collects 10 random numbers using asynchronous understanding"""
-    random_numbers = [num async for num in async_generator()]
-    return random_numbers
+    random_numbers = [num async for num in async_generator()] # Collects 10 random numbers
+    return random_numbers # Returns the 10 random numbers
--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -8,7 +8,7 @@ async_comprehension = __import__("1-async_comprehension").async_comprehension
 
 async def measure_runtime() -> float:
     """Measure the runtime of the async generator"""
-    start = time.time()
-    await asyncio.gather(*(async_comprehension() for i in range(4)))
-    end = time.time()
-    return end - start
+    start = time.time() # Enregistre le temps de départ
+    await asyncio.gather(*(async_comprehension() for i in range(4))) # Appelle async_comprehension 4 fois
+    end = time.time() # Enregistre le temps de fin
+    return end - start # Retourne le temps d'exécution total
