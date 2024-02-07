1a2a331eaae4bd693e552f7a6152576dac1c078e
hug0-cstrs <hugo.casteras@outlook.fr>
2024-02-07 14:57:25 +0000

    add comments for a better understanding

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -7,19 +7,19 @@ class FIFOCache(BaseCaching):
     """FIFO Cache"""
     def __init__(self):
         """Constructor"""
-        super().__init__()
-        self.queue = []
+        super().__init__() # call init from parent class
+        self.queue = [] # create queue
 
     def put(self, key, item):
         """Add an item in the cache"""
-        if key and item:
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                removed = self.queue.pop(0)
-                del self.cache_data[removed]
-                print("DISCARD: {}".format(removed))
-            self.queue.append(key)
-            self.cache_data[key] = item
+        if key and item: # if key and item are not None
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS: # if cache is at max capacity
+                removed = self.queue.pop(0) # remove first item from queue
+                del self.cache_data[removed] # remove first item from cache
+                print("DISCARD: {}".format(removed)) # print message
+            self.queue.append(key) # add key to queue
+            self.cache_data[key] = item # add key and item to cache
 
     def get(self, key):
         """Get an item by key"""
-        return self.cache_data.get(key, None)
+        return self.cache_data.get(key, None) # return item from cache if key exists, else None
