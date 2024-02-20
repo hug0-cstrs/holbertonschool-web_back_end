b63cbc619a8b4530b350dccd65f54f7fed82362a
hug0-cstrs <hugo.casteras@outlook.fr>
2024-02-20 11:14:10 +0000

    Refactor filter_datum function to use regular expressions for obfuscation

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python3
 """ module for filtered_logger """
 
+import re
+
 
 def filter_datum(fields, redaction, message, separator):
-    """
-    Filters and redacts sensitive data from a message
-    """
-    for field in fields:  # for each field in fields
-        start = message.find(field + "=")  # find the start of the field
-        end = message.find(separator, start)  # find the end of the field
-        if start != -1 and end != -1:  # if the field is found
-            end += len(separator)  # add the length of the separator to the end
-            message = message[:start] + field + "=" + \
-                redaction + separator + message[end:]  # redact the field
-    return message  # return the redacted message
+    """ returns the log message obfuscated """
+    regex = r'({}=)(.*?)(?={}|$)'.format('|'.join(fields), separator)
+    return re.sub(regex, lambda x: '{}={}'.format(x.group(1),
+                                                  redaction), message)
