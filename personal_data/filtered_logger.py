208baacccff42e32aa9dbecb4aee45b666d1efd0
hug0-cstrs <hugo.casteras@outlook.fr>
2024-02-20 14:29:39 +0000

    Add RedactingFormatter class to handle personal data filtering

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -2,7 +2,7 @@
 """
 Module for handling Personal Data
 """
-
+import logging
 import re
 from typing import List
 
@@ -14,3 +14,22 @@ def filter_datum(fields: List[str], redaction: str,
         message = re.sub(f'{f}=.*?{separator}',
                          f'{f}={redaction}{separator}', message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """ Constructor method """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Filters values in incoming log records """
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
