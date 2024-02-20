29be9c4828ffe58c517e243ebde681155f7d2d4e
hug0-cstrs <hugo.casteras@outlook.fr>
2024-02-20 10:59:12 +0000

    Refactor filter_datum function to improve redaction logic

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,8 +3,14 @@
 
 
 def filter_datum(fields, redaction, message, separator):
-    """ returns the log message obfuscated """
-    for field in fields:
-        message = message.replace(field + separator,
-                                  field + redaction + separator)
-    return message
+    """
+    Filters and redacts sensitive data from a message
+    """
+    for field in fields:  # for each field in fields
+        start = message.find(field + "=")  # find the start of the field
+        end = message.find(separator, start)  # find the end of the field
+        if start != -1 and end != -1:  # if the field is found
+            end += len(separator)  # add the length of the separator to the end
+            message = message[:start] + field + "=" + \
+                redaction + separator + message[end:]  # redact the field
+    return message  # return the redacted message
