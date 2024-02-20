286ebc82c0c653c6bbb54420bc342cc00cbbbf31
hug0-cstrs <hugo.casteras@outlook.fr>
2024-02-20 19:27:36 +0000

    Add MySQL database connection functionality

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -4,7 +4,10 @@ Module for handling Personal Data
 """
 import logging
 import re
+from os import environ
 from typing import List
+
+import mysql.connector
 
 PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
@@ -45,3 +48,17 @@ def get_logger() -> logging.Logger:
     stream.setFormatter(RedactingFormatter(list(PII_FIELDS)))
     logger.addHandler(stream)
     return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """ Returns a connector to a MySQL database """
+    username = environ.get("PERSONAL_DATA_DB_USERNAME", "root")
+    password = environ.get("PERSONAL_DATA_DB_PASSWORD", "")
+    host = environ.get("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = environ.get("PERSONAL_DATA_DB_NAME")
+
+    cnx = mysql.connector.connection.MySQLConnection(user=username,
+                                                     password=password,
+                                                     host=host,
+                                                     database=db_name)
+    return cnx
