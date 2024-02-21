6784f831b21cbcdea8e46acdd141f45e7c8f5b95
hug0-cstrs <hugo.casteras@outlook.fr>
2024-02-21 20:54:13 +0000

    10. Basic - User object

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 """ Module of Basic Authentication"""
 
-from api.v1.auth.auth import Auth
 from base64 import b64decode
+from typing import TypeVar
+
+from api.v1.auth.auth import Auth
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -48,3 +51,25 @@ class BasicAuth(Auth):
                 ":" not in decoded_base64_authorization_header):
             return (None, None)
         return tuple(decoded_base64_authorization_header.split(":", 1))
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """ Method that returns the User instance
+        based on his email and password
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        try:
+            found_users = User.search({'email': user_email})
+        except Exception:
+            return None
+
+        for user in found_users:
+            if user.is_valid_password(user_pwd):
+                return user
+
+        return None
