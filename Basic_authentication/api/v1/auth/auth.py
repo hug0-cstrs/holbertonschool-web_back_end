1f11fdcaa749fa1013272f5eef036e5200d2b98f
hug0-cstrs <hugo.casteras@outlook.fr>
2024-02-21 21:11:56 +0000

    13. Require auth with stars

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -18,8 +18,12 @@ class Auth:
         if not path.endswith('/'):
             path = path + '/'
 
-        if path in excluded_paths:
-            return False
+        for excluded_path in excluded_paths:
+            if excluded_path.endswith('*') and
+            path.startswith(excluded_path[:-1]):
+                return False
+            if path == excluded_path:
+                return False
         return True
 
     def authorization_header(self, request=None) -> str:
