db4d5c06b24b3af5607d5ba6d3b9374c49c63acd
hug0-cstrs <hugo.casteras@outlook.fr>
2024-02-21 20:16:41 +0000

    6. Basic auth

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -1,23 +1,25 @@
-#!/usr/bin/env python3
+#!/usr/bin/ python3
 """
 Route module for the API
 """
-from os import getenv
-from api.v1.views import app_views
-from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
 import os
+from os import getenv
 
+from api.v1.views import app_views
+from flask import Flask, abort, jsonify, request
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+AUTH_TYPE = getenv("AUTH_TYPE")
 
-
-auth = None
-if getenv("AUTH_TYPE") == "auth":
+if AUTH_TYPE == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
+elif AUTH_TYPE == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
 
 
 @app.errorhandler(404)
---
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,7 @@
+#!/usr/bin/ python3
+
+from api.v1.auth.auth import Auth
+
+class BasicAuth(Auth):
+  """ Class to manage the API authentication """
+  pass
