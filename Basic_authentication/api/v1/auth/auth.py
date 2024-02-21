1a69fefff7b34258dd19bfecd9dafa8d3fa46d2a
hug0-cstrs <hugo.casteras@outlook.fr>
2024-02-21 19:33:22 +0000

    4. Define which routes don't need authentication

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -15,6 +15,9 @@ class Auth:
         if path is None or excluded_paths is None or excluded_paths == []:
             return True
 
+        if not path.endswith('/'):
+            path = path + '/'
+
         if path in excluded_paths:
             return False
         return True
