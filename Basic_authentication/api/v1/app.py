0385356a37215da60764ff30f3a6089626efadbe
hug0-cstrs <hugo.casteras@outlook.fr>
2024-02-21 19:18:17 +0000

    Fix error message in forbidden handler

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -32,7 +32,7 @@ def unauthorized(error) -> str:
 def forbidden(error) -> str:
     """Forbidden handler
     """
-    return jsonify({"errors": "Forbidden"}), 403
+    return jsonify({"error": "Forbidden"}), 403
 
 
 if __name__ == "__main__":
