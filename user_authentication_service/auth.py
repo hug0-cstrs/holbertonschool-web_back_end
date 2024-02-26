c356b06a240e9d78b2f05445e1ebf290d5453383
hug0-cstrs <hugo.casteras@outlook.fr>
2024-02-26 12:57:16 +0000

    5. Register user

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -3,6 +3,7 @@
 
 import bcrypt
 from db import DB
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
 from user import User
 
@@ -11,3 +12,21 @@ def _hash_password(password: str) -> str:
     """ _hash_password: returns a salted hash of the input password
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ register_user: returns a User object
+        """
+        try:
+            # Check if user already exists
+            self._db.find_user_by(email=email)
+            raise ValueError(f'User {email} already exists')
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
