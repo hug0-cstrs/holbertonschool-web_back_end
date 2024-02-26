c51593a675ba97daf2a8decdb3912ccd77619fd7
hug0-cstrs <hugo.casteras@outlook.fr>
2024-02-26 13:05:02 +0000

    7. Register user

---
+++ user_authentication_service/app.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+""" Main file
+"""
+
+from auth import Auth
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+AUTH = Auth()
+
+
+@app.route('/')
+def welcome():
+    return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'])
+def users():
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
