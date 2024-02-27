fdd156c333b8deb36d5bd32da9e9eb8c8ef37fbf
hug0-cstrs <hugo.casteras@outlook.fr>
2024-02-27 12:32:10 +0000

    14. Log out

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -45,5 +45,17 @@ def login():
         abort(401)
 
 
+@app.route('/sessions', methods=['DELETE'])
+def logout():
+    """ DELETE /sessions"""
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        AUTH.destroy_session(user.id)
+        return jsonify({"message": "Bienvenue"})
+    else:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
