223b0f3b369822b4045dfa61b54bde28460d4198
hug0-cstrs <hugo.casteras@outlook.fr>
2024-02-27 12:47:13 +0000

    18. Update password

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -90,3 +90,13 @@ class Auth:
             return reset_token
         except NoResultFound:
             raise ValueError
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """ update_password: updates the user's hashed password
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            self._db.update_user(user.id, _hash_password(password),
+                                 reset_token=None)
+        except NoResultFound:
+            raise ValueError
