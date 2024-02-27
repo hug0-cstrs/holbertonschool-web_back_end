42b068278a1cef9b648b29b3a12d91746feab48e
hug0-cstrs <hugo.casteras@outlook.fr>
2024-02-27 10:16:12 +0000

    10. Get session ID

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -49,3 +49,14 @@ class Auth:
                                   user.hashed_password)
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        """ create_session: returns the session ID as a string
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None
