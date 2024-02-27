40c88cf4db2e40985809c5627500f8e550cad43e
hug0-cstrs <hugo.casteras@outlook.fr>
2024-02-27 12:54:27 +0000

    update file

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -85,6 +85,7 @@ def update_password():
     email = request.form.get('email')
     reset_token = request.form.get('reset_token')
     new_password = request.form.get('new_password')
+
     try:
         AUTH.update_password(reset_token, new_password)
         return jsonify({"email": email, "message": "Password updated"}), 200
