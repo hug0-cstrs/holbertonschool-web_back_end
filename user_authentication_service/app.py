94e386d4829c78102948ee93fada66fd09421772
hug0-cstrs <hugo.casteras@outlook.fr>
2024-02-27 12:12:35 +0000

    update file

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -16,23 +16,20 @@ def welcome():
 
 @app.route('/users', methods=['POST'])
 def users():
+    """ POST /users"""
     email = request.form.get('email')
     password = request.form.get('password')
 
     try:
-        if AUTH.valid_login(email, password):
-            session_id = AUTH.create_session(email)
-            response = jsonify({"email": email, "message": "logged in"})
-            response.set_cookie('session_id', session_id)
-            return response
-        else:
-            abort(401)
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
     except ValueError:
-        abort(401)
+        return jsonify({"message": "email already registered"}), 400
+
 
 @app.route('/sessions', methods=['POST'])
 def login():
-    """ POST /sessions """
+    """ POST /sessions"""
     email = request.form.get('email')
     password = request.form.get('password')
 
@@ -46,6 +43,7 @@ def login():
             abort(401)
     except ValueError:
         abort(401)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
