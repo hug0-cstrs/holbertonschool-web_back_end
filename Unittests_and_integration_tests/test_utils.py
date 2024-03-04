37576eb2ff77de3e30ba1c6bcc8d9890b4eeb0a7
hug0-cstrs <hugo.casteras@outlook.fr>
2024-03-04 15:05:49 +0000

    update file

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -62,10 +62,12 @@ class TestMemoize(unittest.TestCase):
             """ Test Class for wrapping with memoize """
 
             def a_method(self):
+                """ A method that returns 42 """
                 return 42
 
             @memoize
             def a_property(self):
+                """ A property that returns a_method """
                 return self.a_method()
 
         with patch.object(TestClass, 'a_method') as mock:
