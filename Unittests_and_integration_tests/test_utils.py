ccdd61839939cb4f670f653b6733556041b3eed4
hug0-cstrs <hugo.casteras@outlook.fr>
2024-03-04 13:57:05 +0000

    Add test cases for access_nested_map function

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -14,9 +14,11 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, nested_map, path, expected):
+        """ Test access_nested_map """
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
     def test_access_nested_map_exception(self):
+        """ Test access_nested_map with exception """
         nested_map = {"a": {"b": 2}}
         path = ("a", "c")
         with self.assertRaises(KeyError) as e:
