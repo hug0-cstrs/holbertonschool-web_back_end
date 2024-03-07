86786777bdcb393941a3c819a6803ef84604d897
hug0-cstrs <hugo.casteras@outlook.fr>
2024-03-07 10:14:14 +0000

    Update i18n configuration

--- i18n/1-app.py
+++ i18n/1-app.py
@@ -6,9 +6,9 @@ from flask_babel import Babel
 
 class Config:
     """ Config class """
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
 app = Flask(__name__)
@@ -27,6 +27,7 @@ def hello():
       Render template
     """
     return render_template('0-index.html')
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
