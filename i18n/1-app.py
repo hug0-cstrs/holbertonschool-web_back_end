3d54129078ff0045257741a67998f9d211f56c2c
hug0-cstrs <hugo.casteras@outlook.fr>
2024-03-07 10:11:59 +0000

    Add render_template and update Flask app configuration

--- i18n/1-app.py
+++ i18n/1-app.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python3
 """ Basic Flask app """
-from flask import Flask
+from flask import Flask, render_template
 from flask_babel import Babel
-
-
-app = Flask(__name__)
-babel = Babel(app)
 
 
 class Config:
@@ -15,5 +11,22 @@ class Config:
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
+app = Flask(__name__)
+app.url_map.strict_slashes = False
 app.config.from_object(Config)
-babel.init_app(app)
+
+babel = Babel(app)
+Babel.default_locale = 'en'
+Babel.default_timezone = 'UTC'
+
+
+@app.route('/', methods=['GET'])
+def hello():
+    """ GET /
+    Return:
+      Render template
+    """
+    return render_template('0-index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
