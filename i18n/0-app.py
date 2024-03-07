9771f59623c015d40df89c4127cd2a3ea5bc067d
hug0-cstrs <hugo.casteras@outlook.fr>
2024-03-07 09:54:30 +0000

    0. Basic Flask app

---
+++ i18n/0-app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" Basic Flask app """
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/', methods=['GET'])
+def hello():
+    """ GET /
+    Return:
+      - Render template
+    """
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
