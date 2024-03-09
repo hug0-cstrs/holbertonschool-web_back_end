8c32b9515477fc151d24c66e26a17c3b2acb189a
hug0-cstrs <hugo.casteras@outlook.fr>
2024-03-09 09:02:29 +0000

    update file

--- i18n/4-app.py
+++ i18n/4-app.py
@@ -1,46 +1,45 @@
 #!/usr/bin/env python3
 """
-Flask app
+Basic Babel setup
 """
+
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
-
-class Config:
-    """
-    Config class
-    """
-    LANGUAGES = ['en', 'fr']
-
-
 app = Flask(__name__)
-app.url_map.strict_slashes = False
-app.config.from_object(Config)
-
 babel = Babel(app)
-Babel.default_locale = 'en'
-Babel.default_timezone = 'UTC'
 
 
-@app.route('/', methods=['GET'])
-def hello():
-    """ GET /
-    Return:
-      - Render template
+class Config(object):
     """
-    return render_template('4-index.html')
+    Config class for Babel
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object("3-app.Config")
+
+
+@app.route("/")
+def index():
+    """
+    function that render index.html template
+    """
+    return render_template("3-index.html")
 
 
 @babel.localeselector
 def get_locale():
     """
-    Get locale from request
+    Get the current locale
     """
-    locale = request.args.get('locale')
-    if locale in Config.LANGUAGES:
+    locale = request.args.get("locale")
+    if locale and locale in app.config["LANGUAGES"]:
         return locale
-    return request.accept_languages.best_match(Config.LANGUAGES)
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host="0.0.0.0", port=5000)
