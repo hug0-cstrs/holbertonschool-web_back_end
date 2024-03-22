0a6dbab09715b91e8099084181422d561c0c9585
hug0-cstrs <hugo.casteras@outlook.fr>
2024-03-22 19:26:59 +0000

    2. Incrementing values

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -2,7 +2,8 @@
 """ Main file """
 
 import uuid
-from typing import Union, Optional, Callable
+from functools import wraps
+from typing import Callable, Optional, Union
 
 import redis
 
@@ -21,7 +22,8 @@ class Cache:
         self._redis.set(key, data)
         return key
 
-    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[
+            str, bytes, int, float]:
         """ Retrieve data from redis """
         data = self._redis.get(key)
         if data is None:
@@ -37,3 +39,19 @@ class Cache:
     def get_int(self, key: str) -> Optional[int]:
         """ Retrieve integer data from redis """
         return self.get(key, int)
+
+    def count_calls(method: Callable) -> Callable:
+        """ Decorator to count method calls """
+        @wraps(method)
+        def wrapper(self, *args, **kwargs):
+            key = method.__qualname__
+            self._redis.incr(key)
+            return method(self, *args, **kwargs)
+        return wrapper
+
+    @count_calls
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """ Store data in redis """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
