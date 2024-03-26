7aa96ff5306167c3c23fc9ea678d4155c037a08a
hug0-cstrs <hugo.casteras@outlook.fr>
2024-03-26 09:53:37 +0000

    update file

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -1,20 +1,26 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul.js');
 
-describe('calculateNumber', () => {
-  it('checks the output', () => {
+describe('calculateNumber', function() {
+  it('should return 4', () => {
     assert.strictEqual(calculateNumber(1, 3), 4);
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
-    assert.strictEqual(calculateNumber(3.7, 1), 5);
-    assert.strictEqual(calculateNumber(3.7, 1.2), 5);
   });
+  it('should return 5', () => {
+    assert.strictEqual(calculateNumber(1.9, 3), 5);
+  });
+  it('should return 5', () => {
+    assert.strictEqual(calculateNumber(1.4, 3.5), 5);
+  });
+  it('should return 6', () => {
+    assert.strictEqual(calculateNumber(1.4, 3.5), 5);
+  });
+
   it('negative numbers', () => {
     assert.strictEqual(calculateNumber(-1, 1), 0);
     assert.strictEqual(calculateNumber(1, -1), 0);
     assert.strictEqual(calculateNumber(-1, -1), -2);
   });
+
   it('checks arguments', () => {
     assert.strictEqual(isNaN(calculateNumber(1)), true);
     assert.strictEqual(isNaN(calculateNumber()), true);
