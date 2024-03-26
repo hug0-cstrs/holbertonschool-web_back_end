c87e56cd46c2f6cb261c7c9cfc19b76cb018a5bc
hug0-cstrs <hugo.casteras@outlook.fr>
2024-03-26 14:24:14 +0000

    6. Async tests with done

---
+++ unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,14 @@
+const chai = require('chai');
+const getPaymentTokenFromAPI = require('./6-payment_token');
+
+describe('getPaymentTokenFromAPI', () => {
+  it('should return a payment token when API call is successful', (done) => {
+    getPaymentTokenFromAPI(true)
+      .then((res) => {
+        chai.expect(res).to.have.property('data');
+        chai.expect(res.data).to.equal('Successful response from the API');
+        done();
+      })
+      .catch((err) => done(err));
+  });
+});
