f5ec38e9e26a7d455b129c768fa6993f18c2c3fa
hug0-cstrs <hugo.casteras@outlook.fr>
2024-03-26 14:10:57 +0000

    5. Hooks

---
+++ unittests_in_js/5-payment.test.js
@@ -0,0 +1,27 @@
+const sendPaymentRequestToApi = require('./5-payment.js');
+const sinon = require('sinon');
+const { expect } = require('chai');
+
+describe('sendPaymentRequestToApi', () => {
+  let consoleSpy;
+
+  beforeEach(() => {
+    consoleSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    consoleSpy.restore();
+  });
+
+  it('should log "The total is: 120" and be called once when called with 100 and 20', () => {
+    sendPaymentRequestToApi(100, 20);
+    expect(consoleSpy.calledOnce).to.be.true;
+    expect(consoleSpy.calledWith('The total is: 120')).to.be.true;
+  });
+
+  it('should log "The total is: 20" and be called once when called with 10 and 10', () => {
+    sendPaymentRequestToApi(10, 10);
+    expect(consoleSpy.calledOnce).to.be.true;
+    expect(consoleSpy.calledWith('The total is: 20')).to.be.true;
+  });
+});
